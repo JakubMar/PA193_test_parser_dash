94f57133b871eb02bccf90f29f2c4312e6e518d0
JakubMar <kubo.martinka@gmail.com>
2017-10-09 17:26:59 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
 
 using namespace std;
-
 int main()
 {
     cout << "Hello World!" << endl;
     return 0;
 }
 
+/*
+char buffer[] = {0x00,0x11};
+
+buffer = read_resources();
+block = parse(buffer);
+if(block)
+{
+if(valid(block))
+    {
+     print(block);
+    }
+}
+*/

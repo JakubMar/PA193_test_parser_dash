acfed68baf19156fec24725a3c7b79ba283e1b79
JakubMar <xmartink@mail.muni.cz>
2017-11-06 23:35:41 +0100

    Doxygen config files

---
+++ html/search/searchdata.js
@@ -0,0 +1,18 @@
+var indexSectionsWithContent =
+{
+  0: "btv",
+  1: "btv"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes"
+};
+
